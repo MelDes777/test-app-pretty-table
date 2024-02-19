07a68c1c7e4c34c17d6fc6c1a8a280d82639dee9
Strakatov Nikolay <desux327@gmail.com>
2024-02-19 14:23:02 +0600

    Part4

--- src/pretty-table/pretty-table.component.ts
+++ src/pretty-table/pretty-table.component.ts
@@ -10,9 +10,10 @@ import { UserData } from '../models/UserData';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {DatePipe, CommonModule} from '@angular/common';
 import { FormControl, FormGroup } from '@angular/forms';
-import { BehaviorSubject, Observable, merge } from 'rxjs';
+import { BehaviorSubject, Observable, merge, of } from 'rxjs';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { switchMap, catchError, finalize, delay, tap, debounceTime, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pretty-table',
@@ -36,19 +37,11 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 export class PrettyTableComponent implements AfterViewInit, OnInit {
 
   displayedColumns: string[] = [
-    'id',
-    'firstName',
-    'lastName',
-    'isActive',
-    'balance',
-    'picture',
-    'age',
-    'company',
-    'email',
-    'address',
-    'tags',
-    'favoriteFruit',
+    'id', 'firstName', 'lastName', 'isActive', 'balance', 'picture', 'age',
+    'company', 'email', 'address', 'tags', 'favoriteFruit',
   ];
+
+  isLoadingResults = false;
 
   dataSource: UserData[] = [];
   dataSourceFiltered!: MatTableDataSource<UserData>;
@@ -75,148 +68,81 @@ export class PrettyTableComponent implements AfterViewInit, OnInit {
     favoriteFruit: new FormControl(false),
   });
 
-  id = this.form.get('id');
-  firstName = this.form.get('firstName');
-  lastName = this.form.get('lastName');
-  isActive = this.form.get('isActive');
-  balance = this.form.get('balance');
-  picture = this.form.get('picture');
-  age = this.form.get('age');
-  company = this.form.get('company');
-  email = this.form.get('email');
-  address = this.form.get('address');
-  tags = this.form.get('tags');
-  favoriteFruit = this.form.get('favoriteFruit');
-
   columnDefinitions = [
-    { def: 'id', label: 'ID', hide: this.id ? this.id.value : false },
-    {
-      def: 'firstName',
-      label: 'FirstName',
-      hide: this.firstName ? this.firstName.value : false,
-    },
-    {
-      def: 'lastName',
-      label: 'LastName',
-      hide: this.lastName ? this.lastName.value : false,
-    },
-    {
-      def: 'isActive',
-      label: 'isActive',
-      hide: this.isActive ? this.isActive.value : false,
-    },
-    {
-      def: 'balance',
-      label: 'Balance',
-      hide: this.balance ? this.balance.value : false,
-    },
-    {
-      def: 'picture',
-      label: 'Picture',
-      hide: this.picture ? this.picture.value : false,
-    },
-    { def: 'age', label: 'Age', hide: this.age ? this.age.value : false },
-    {
-      def: 'company',
-      label: 'Company',
-      hide: this.company ? this.company.value : false,
-    },
-    {
-      def: 'email',
-      label: 'Email',
-      hide: this.email ? this.email.value : false,
-    },
-    {
-      def: 'address',
-      label: 'Address',
-      hide: this.address ? this.address.value : false,
-    },
-    { def: 'tags', label: 'Tags', hide: this.tags ? this.tags.value : false },
-    {
-      def: 'favoriteFruit',
-      label: 'FavoriteFruit',
-      hide: this.favoriteFruit ? this.favoriteFruit.value : false,
-    },
-  ].map(column => ({
-    ...column,
-    hide: column.hide !== null ? column.hide : false
-}));;
+    { def: 'id', label: 'ID' },
+    { def: 'firstName', label: 'FirstName' },
+    { def: 'lastName', label: 'LastName' },
+    { def: 'isActive', label: 'isActive' },
+    { def: 'balance', label: 'Balance' },
+    { def: 'picture', label: 'Picture' },
+    { def: 'age', label: 'Age' },
+    { def: 'company', label: 'Company' },
+    { def: 'email', label: 'Email' },
+    { def: 'address', label: 'Address' },
+    { def: 'tags', label: 'Tags' },
+    { def: 'favoriteFruit', label: 'FavoriteFruit' },
+  ];
 
-getDisplayedColumns() {
-  this.columns = this.columnDefinitions
-    .filter((cd) => !cd.hide)
-    .map((cd) => cd.def);
-}
+formControls = Object.values(this.form.controls) as FormControl[];
+
+
 
 ngOnInit(): void {
+
   this.processMockData();
-  this.dataSourceFiltered = new MatTableDataSource(this.dataSource);
+  this.isLoadingResults = true;
+
+   of(null).pipe(
+    mergeMap(() => of(null).pipe(delay(this.getRandomDelay())))
+  ).subscribe(() => {
+    this.dataSourceFiltered = new MatTableDataSource(this.dataSource);
+    this.dataSourceFiltered.paginator = this.paginator;
+    this.dataSourceFiltered.sort = this.sort;
+    this.displayedColumns = this.columns;
+    this.getDisplayedColumns();
+    this.isLoadingResults = false;
+  });
 }
 
 ngAfterViewInit() {
-
-  let o1: Observable<boolean> = this.id!.valueChanges;
-  let o2: Observable<boolean> = this.firstName!.valueChanges;
-  let o3: Observable<boolean> = this.lastName!.valueChanges;
-  let o4: Observable<boolean> = this.isActive!.valueChanges;
-  let o5: Observable<boolean> = this.balance!.valueChanges;
-  let o6: Observable<boolean> = this.picture!.valueChanges;
-  let o7: Observable<boolean> = this.age!.valueChanges;
-  let o8: Observable<boolean> = this.company!.valueChanges;
-  let o9: Observable<boolean> = this.email!.valueChanges;
-  let o10: Observable<boolean> = this.address!.valueChanges;
-  let o11: Observable<boolean> = this.tags!.valueChanges;
-  let o12: Observable<boolean> = this.favoriteFruit!.valueChanges;
-
-  merge(o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12)
-    .subscribe((v) => {
-      this.columnDefinitions[0].hide = this.id!.value;
-      this.columnDefinitions[1].hide = this.firstName!.value;
-      this.columnDefinitions[2].hide = this.lastName!.value;
-      this.columnDefinitions[3].hide = this.isActive!.value;
-      this.columnDefinitions[4].hide = this.balance!.value;
-      this.columnDefinitions[5].hide = this.picture!.value;
-      this.columnDefinitions[6].hide = this.age!.value;
-      this.columnDefinitions[7].hide = this.company!.value;
-      this.columnDefinitions[8].hide = this.email!.value;
-      this.columnDefinitions[9].hide = this.address!.value;
-      this.columnDefinitions[10].hide = this.tags!.value;
-      this.columnDefinitions[11].hide = this.favoriteFruit!.value;
-
-      console.log('Column definitions:', this.columnDefinitions);
-
+  merge(...this.formControls.map(control => control.valueChanges.pipe(debounceTime(300))))
+    .subscribe(() => {
       this.getDisplayedColumns();
       this.displayedColumns = this.columns;
-  });
+    });
 
   this.getDisplayedColumns();
 
-  this.dataSourceFiltered.paginator = this.paginator;
-  this.dataSourceFiltered.sort = this.sort;
 }
 
-  processMockData(): void {
-    this.dataSource = mockData.map((data) => {
-      const processedData: UserData = {
-        _id: data._id,
-        isActive: data.isActive || false, // Присваиваем false, если значение не определено
-        balance: data.balance || '', // Присваиваем пустую строку, если значение не определено
-        picture: data.picture || '', // Присваиваем пустую строку, если значение не определено
-        age: data.age || 0, // Присваиваем 0, если значение не определено
-        name: {
-          first: data.name?.first || '', // Присваиваем пустую строку, если значение не определено
-          last: data.name?.last || '', // Присваиваем пустую строку, если значение не определено
-        },
-        company: data.company || '', // Присваиваем пустую строку, если значение не определено
-        email: data.email || '', // Присваиваем пустую строку, если значение не определено
-        address: data.address || '', // Присваиваем пустую строку, если значение не определено
-        tags: data.tags || [], // Присваиваем пустой массив, если значение не определено
-        favoriteFruit: data.favoriteFruit || '', // Присваиваем пустую строку, если значение не определено
-      };
+getDisplayedColumns() {
+  this.columns = this.columnDefinitions
+    .filter(cd => !this.form.get(cd.def)?.value)
+    .map(cd => cd.def);
+}
 
-      return processedData;
-    });
-  }
+processMockData(): void {
+  this.dataSource = mockData.map(data => ({
+    _id: data._id ?? '',
+    isActive: data.isActive ?? false,
+    balance: data.balance ?? '',
+    picture: data.picture ?? '',
+    age: data.age ?? 0,
+    name: {
+      first: data.name?.first ?? '',
+      last: data.name?.last ?? '',
+    },
+    company: data.company ?? '',
+    email: data.email ?? '',
+    address: data.address ?? '',
+    tags: data.tags ?? [],
+    favoriteFruit: data.favoriteFruit ?? '',
+  }));
+}
+
+private getRandomDelay(): number {
+  return Math.floor(Math.random() * 1000) + 1000;
+}
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
